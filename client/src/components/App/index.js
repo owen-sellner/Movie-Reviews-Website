60e26d620d897179d163398a6efc1f4759e2318c
owen-sellner <osellner@uwaterloo.ca>
2022-05-25 09:45:02 -0400

    progress

--- client/src/components/App/index.js
+++ client/src/components/App/index.js
@@ -1,43 +1,84 @@
-import React, { Component } from 'react';
-import {
-  BrowserRouter as Router,
-  Route,
-  Link,
-} from 'react-router-dom';
+import * as React from 'react';
 
-import Home from '../Home';
-import PrivateRoute from '../Navigation/PrivateRoute.js';
+const App = () => {
 
-
-
-class App extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      //
-    };
-  }
-
-  componentDidMount() {
-    //
-  }
+  const recipes = [
+    {
+      title: 'Fruit salad',
+      difficulty: '2',
+      ingredients: ['apple', 'banana', 'blueberries', 'raisins', 'walnuts'],
+      calories: "200",
+      instructions: "Wash fresh fruit. Slice fruit into pieces. Mix all ingredients in a bowl."
+    }, {
+      title: 'Avocado wrap',
+      difficulty: '3',
+      ingredients: ['avocado', 'spinach', 'pine nuts', 'mayo', 'apple', 'tortilla bread'],
+      calories: "400",
+      instructions: "Wash all fruits and vegetables. Slice avocadoes and apples. Mix all ingredients and wrap them in a tortilla bread."
+    },
+  ];
 
 
-  componentWillUnmount() {
-    this.listener();
-  }
 
 
-  render() {
-    return (
-	  <Router>
-	    <div>
-        <PrivateRoute exact path="/" component={Home}/>
-	    </div>
-	  </Router>
-    );
-  }
+  return (
+    <div>
+      <h1>
+        Recipe Finder
+      </h1>
+
+      <Search />
+
+      <hr />
+
+      <List list={recipes} />
+
+    </div>
+  );
 }
+
+function Search() {
+  return (
+    <div>
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" />
+    </div>
+  )
+}
+
+function List(props) {
+  return (
+    <ul>
+      {props.list.map((item) => {
+        return (
+          <Item item={item} />
+        );
+      })}
+    </ul>
+  )
+}
+
+function Item(props) {
+  return (
+    <li>
+      <li>{props.item.title}</li>
+      <ul>
+        <li>Difficulty: {props.item.difficulty}</li>
+        <li>Ingredients:
+          <ul>
+            {props.item.ingredients.map((thing) => {
+              return (
+                <li>{thing}</li>
+              );
+            })}
+          </ul>
+        </li>
+        <li>Calories: {props.item.calories}</li>
+        <li>Instructions: {props.item.instructions}</li>
+      </ul>
+    </li>
+  );
+}
+
 
 export default App;
