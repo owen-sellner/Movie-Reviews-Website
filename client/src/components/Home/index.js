d7f871eb2d90f11e7509104d130e80d64e621147
owen-sellner <osellner@uwaterloo.ca>
2022-06-17 10:37:05 -0400

    mui done

--- client/src/components/Home/index.js
+++ client/src/components/Home/index.js
@@ -10,80 +10,114 @@ import RadioGroup from '@material-ui/core/RadioGroup';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import FormControl from '@material-ui/core/FormControl';
 import FormLabel from '@material-ui/core/FormLabel';
+import MenuItem from '@material-ui/core/MenuItem';
 import Button from '@material-ui/core/Button';
 
 const colSize = 1;
 
+const serverURL = "http://ov-research-4.uwaterloo.ca:3103";
+
 // Parent Component
 function Review() {
+    
+    const [selectedMovie, setSelectedMovie] = React.useState([]);
+    const [enteredTitle, setEnteredTitle] = React.useState([]);
+    const [enteredReview, setEnteredReview] = React.useState([]);
+    const [selectedRating, setSelectedRating] = React.useState([]);
+
+    const handleSubmit = () => {
+        setSelectedMovie(selectedMovie => selectedMovie.concat(newMovie))
+        setEnteredTitle(enteredTitle => enteredTitle.concat(newTitle))
+        setEnteredReview(enteredReview => enteredReview.concat(newReview))
+        setSelectedRating(selectedRating => selectedRating.concat(newRating))
+    }
+
+    const [newMovie, setNewMovie] = React.useState('');
+    const [newTitle, setNewTitle] = React.useState('');
+    const [newReview, setNewReview] = React.useState('');
+    const [newRating, setNewRating] = React.useState('');
+
     return(
         <div style={{margin: 15}}>
             {/* Creates a column grid for the body of the page */}
             <Grid container
                 direction="column"
-                alignItems='center'
+                justifyContent="space-evenly"
+                alignItems="center"
             >
                 {/* Page Title */}
                 <Grid item>
                     <Typography variant="h3">
-                        Movie Reviews
+                        <b>Movie Reviews</b>
                     </Typography> 
                 </Grid>
 
                 {/* Review Form */}
                 <Grid container
+                    alignContent="center"
+                    spacing={2} 
                     direction="row"
                 >   
                     {/* Row 1 */}
-                        <Grid item xs={2*colSize}>
+                        <Grid item xs={4*colSize}></Grid>
+                        <Grid item xs={4*colSize}>
                             <Typography variant="h4">
                                 New Review
                             </Typography> 
                         </Grid>
-                        <Grid item xs={12-2*colSize}></Grid>
+                        <Grid item xs={12-8*colSize}></Grid>
                     {/* Row 2 */}
+                        <Grid item xs={4*colSize}></Grid>
                         <Grid item xs={colSize}>
                             <Typography variant="h6">Select Movie:</Typography>
                         </Grid>
-                        <Grid item xs={2*colSize}>
+                        <Grid item xs={3*colSize}>
                             <MovieSelection />
                         </Grid>
-                        <Grid item xs={12-3*colSize}></Grid>
+                        <Grid item xs={12-8*colSize}></Grid>
                     {/* Row 3 */}
+                        <Grid item xs={4*colSize}></Grid>
                         <Grid item xs={colSize}>
                             <Typography variant="h6">Enter Title:</Typography>
                         </Grid>
-                        <Grid item xs={2*colSize}>
+                        <Grid item xs={3*colSize}>
                             <ReviewTitle />
                         </Grid>
-                        <Grid item xs={12-3*colSize}></Grid>
+                        <Grid item xs={12-8*colSize}></Grid>
                     {/* Row 4 */}
+                        <Grid item xs={4*colSize}></Grid>
                         <Grid item xs={colSize}>
                             <Typography variant="h6">Enter Review:</Typography>
                         </Grid>
-                        <Grid item xs={2*colSize}>
+                        <Grid item xs={3*colSize}>
                             <ReviewBody />
                         </Grid>
-                        <Grid item xs={12-3*colSize}></Grid>
+                        <Grid item xs={12-8*colSize}></Grid>
                     {/* Row 5 */}
+                        <Grid item xs={4*colSize}></Grid>
                         <Grid item xs={colSize}>
                             <Typography variant="h6">Select Rating:</Typography>
                         </Grid>
-                        <Grid item xs={2*colSize}>
+                        <Grid item xs={3*colSize}>
                             <ReviewRating />
                         </Grid>
-                        <Grid item xs={12-3*colSize}></Grid>
+                        <Grid item xs={12-8*colSize}></Grid>
                     {/* Last Row */}
-                        <Grid item xs={colSize}></Grid> 
-                        <Grid item xs={3*colSize}>
-                            <Button variant="contained" color="primary" size="large" disableElevation>Submit</Button>
+                        <Grid item xs={5*colSize}></Grid> 
+                        <Grid item xs={2*colSize}>
+                            <Button fullWidth 
+                                variant="contained" 
+                                color="primary" 
+                                size="large" 
+                                onClick={handleSubmit}
+                                disableElevation>Submit</Button>
                         </Grid>
                 </Grid>
 
                 {/* User Results */}
                 <Grid item>
                     <Typography variant="h4">
-                        ...
+                        - - - - - - - - - - - - - - - - - - - -
                     </Typography> 
                 </Grid>
 
@@ -96,17 +130,16 @@ function Review() {
 function MovieSelection() {
     return(
         <div>
-            {/* BROKEN CODE
-            <FormControl>
+            <FormControl fullWidth>
                 <FormLabel>Select Movie</FormLabel>
-                <Select id="movie-select">
+                <Select fullWidth id="movie-select" variant="outlined" onChange={event => setNewMovie(event.target.value)}>
                     <MenuItem value="Morbius">Morbius</MenuItem>
                     <MenuItem value="Morbius 2">Morbius 2</MenuItem>
                     <MenuItem value="Morbius 3">Morbius 3</MenuItem>
                     <MenuItem value="Morbius 4">Morbius 4</MenuItem>
                     <MenuItem value="Morbius 5">Morbius 5</MenuItem>
                 </Select>
-            </FormControl> */}
+            </FormControl>
         </div>
     );
 }
@@ -115,7 +148,7 @@ function MovieSelection() {
 function ReviewTitle() {
     return(
         <div>
-            <TextField id="review-title" label="Review Title" variant="outlined"/>
+            <TextField fullWidth id="review-title" label="Review Title" variant="outlined" onChange={event => setNewTitle(event.target.value)}/>
         </div>
     );
 }
@@ -124,7 +157,7 @@ function ReviewTitle() {
 function ReviewBody() {
     return(
         <div>
-            <TextField multiline rows={4} id="review-body" label="Review Body" variant="outlined"/>
+            <TextField multiline fullWidth rows={5} id="review-body" label="Review Body" variant="outlined" onChange={event => setNewReview(event.target.value)}/>
         </div>
     );
 }
@@ -135,7 +168,7 @@ function ReviewRating() {
         <div>
             <FormControl>
             <FormLabel>Rating</FormLabel>
-            <RadioGroup row={true}>
+            <RadioGroup row={true} onChange={event => setNewRating(event.target.value)}>
                 <FormControlLabel value="1" control={<Radio />} label="1"/>
                 <FormControlLabel value="2" control={<Radio />} label="2"/>
                 <FormControlLabel value="3" control={<Radio />} label="3"/>
