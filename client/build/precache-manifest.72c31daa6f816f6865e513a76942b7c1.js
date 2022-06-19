21b59fdcafc61e676d9fce988604b48e6b8a3b35
owen-sellner <43042601+owen-sellner@users.noreply.github.com>
2022-06-19 15:45:34 -0400

    Build Upload

---
+++ client/build/precache-manifest.72c31daa6f816f6865e513a76942b7c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16221d5c9a041ebc44ccae12f45acb3b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1a7ccc74f9c1662dc88a",
+    "url": "/static/css/main.462b0bd9.chunk.css"
+  },
+  {
+    "revision": "79ee139f7a58ca761c98",
+    "url": "/static/js/2.bc057e9d.chunk.js"
+  },
+  {
+    "revision": "89a1b2dcd30c03705b2bceeb141b76b6",
+    "url": "/static/js/2.bc057e9d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a7ccc74f9c1662dc88a",
+    "url": "/static/js/main.da0795ea.chunk.js"
+  },
+  {
+    "revision": "f2401c040861091065be",
+    "url": "/static/js/runtime-main.66f6ec9d.js"
+  }
+]);
