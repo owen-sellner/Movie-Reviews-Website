e47bcd4878f4e2b7293326dbbabe1412706a274a
owen-sellner <osellner@uwaterloo.ca>
2022-06-21 23:01:03 -0400

    complete app

--- client/src/components/App/index.js
+++ client/src/components/App/index.js
@@ -1,44 +1,43 @@
+import React from 'react';
 
-import React, { Component } from 'react';
-import {
-  BrowserRouter as Router,
-  Route,
-  Link,
-} from 'react-router-dom';
+function App () {
+  const textbooks = [{
+    title: 'Book1',
+    author: 'John Doe',
+    year: '3012',
+    url: "https://www.youtube.com/"
+  }, {
+    title: 'Book2',
+    author: 'Jane Doe',
+    year: '3004',
+    url: "https://youtu.be/dQw4w9WgXcQ"
+  }]
 
-import Home from '../Home';
-import PrivateRoute from '../Navigation/PrivateRoute.js';
+  return (
+    <div style={{margin: 15}}>
+      <h1>Lab 3 Solution</h1>
 
+      <Books textbooks={textbooks} />
 
+    </div>
+  )
+}
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      //
-    };
-  }
-
-  componentDidMount() {
-    //
-  }
-
-
-  componentWillUnmount() {
-    this.listener();
-  }
-
-
-  render() {
-    return (
-	  <Router>
-	    <div>
-        <PrivateRoute exact path="/" component={Home}/>
-	    </div>
-	  </Router>
-    );
-  }
+function Books({textbooks}) {
+  return (
+    <div>
+      {textbooks.map((item) => {
+        return(
+          <ul>
+            <li>Title: {item.title}</li>
+            <p>Author: {item.author}</p>
+            <p>Year: {item.year}</p>
+            <p>URL: <a href={item.url}>{item.url}</a></p>
+          </ul>
+        )
+      })}
+    </div>
+  )
 }
 
 export default App;
