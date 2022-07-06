f4e177e1e05b44f70038f05d599916215e12a9e9
owen-sellner <osellner@uwaterloo.ca>
2022-07-06 08:49:14 -0400

    test1

--- server.js
+++ server.js
@@ -13,11 +13,53 @@ app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 
 app.use(express.static(path.join(__dirname, "client/build")));
 
+const recipes = [
+	{
+		title: 'Fruit Salad',
+		difficulty: '2',
+		ingredients: ['apple', 'banana', 'blueberries', 'raisins', 'walnuts'],
+		calories: "200",
+		instructions: "Wash fresh fruit. Slice fruit into pieces. Mix all ingredients in a bowl.",
+		recipeID: 1,
+	}, {
+		title: 'Avocado Wrap',
+		difficulty: '3',
+		ingredients: ['avocado', 'spinach', 'pine nuts', 'mayo', 'apple', 'tortilla bread'],
+		calories: "400",
+		instructions: "Wash all fruits and vegetables. Slice avocados and apples. Mix all ingredients and wrap them in a tortilla bread.",
+		recipeID: 2
+	},
+];
+
+
+app.post('/api/loadRecipes', (req, res) => {
+	let string = JSON.stringify(recipes);
+	console.log(string);
+	res.send({ express: string });
+});
+
+app.post('/api/findRecipe', (req, res) => {
+	let calorieSearchTerm = req.body.calorieSearchTerm;
+	console.log("calorieSearchTerm: ", calorieSearchTerm);
+
+	const foundRecipesByCalorie = recipes.filter(function (recipe) {
+		if (calorieSearchTerm) {
+		  return parseInt(recipe.calories) <= parseInt(calorieSearchTerm);
+		} else {
+		  return recipe;
+		}
+	  });
+
+	let string = JSON.stringify(foundRecipesByCalorie);
+	console.log(string);
+	res.send({ express: string });
+});
 
 app.post('/api/loadUserSettings', (req, res) => {
 
 	let connection = mysql.createConnection(config);
 	let userID = req.body.userID;
+	console.log("UserID: ", userID);
 
 	let sql = `SELECT mode FROM user WHERE userID = ?`;
 	console.log(sql);
@@ -30,13 +72,14 @@ app.post('/api/loadUserSettings', (req, res) => {
 		}
 
 		let string = JSON.stringify(results);
-		//let obj = JSON.parse(string);
+		let obj = JSON.parse(string);
 		res.send({ express: string });
 	});
 	connection.end();
+
 });
 
 
 
 app.listen(port, () => console.log(`Listening on port ${port}`)); //for the dev version
-//app.listen(port, '129.97.25.211'); //for the deployed version, specify the IP address of the server
+//app.listen(3000, '129.97.25.211'); //for the deployed version, specify the IP address of the server
